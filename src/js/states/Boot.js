d17eae9c77ecc7a31fd37390010775018758cc00
Lisandro Lorea <lisandrolorea@gmail.com>
2016-03-07 06:56:05 -0300

    Changed resize behavior

--- src/js/states/Boot.js
+++ src/js/states/Boot.js
@@ -17,9 +17,11 @@ IGJ.Boot.prototype = {
         this.game.state.add('Preloader', IGJ.Preloader);
         this.game.state.add('Game', IGJ.Game);
 
-        this.game.scale.scaleMode = Phaser.ScaleManager.NOSCALE;
+        this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         this.game.scale.minWidth = 320;
         this.game.scale.minHeight = 180;
+        this.game.scale.maxWidth = 1920;
+        this.game.scale.maxHeight = 1080;
         this.game.scale.pageAlignHorizontally = true;
         this.game.scale.pageAlignVertically = false;
         this.game.state.start('Preloader');
